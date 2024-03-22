1d884aeb1af997d28940dd208113df4b1e980347
Jorge Soto <jls@jorgeluissoto.com>
2024-03-21 22:37:26 -0400

    Day 4 done...Rock, Paper, Scissor game done

--- projects/rock_paper_scissors.py
+++ projects/rock_paper_scissors.py
@@ -26,3 +26,45 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
+import random
+
+user_pick = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+computer_pick = random.randint(0,2)
+
+# User pick
+if user_pick == 0:
+  print(rock)
+elif user_pick == 1:
+  print(paper)
+else:
+  print(scissors)
+
+print(f"Computer Chose:")
+#print(computer_pick)
+
+# Computer pick
+if computer_pick == 0:
+  print(rock)
+elif computer_pick == 1:
+  print(paper)
+else:
+  print(scissors)
+
+# Logic to find winner
+if user_pick >= 3 or user_pick <0:
+  print("You typed an invalid number, you lose!")
+else:
+    if user_pick == 0 and computer_pick == 2:
+       print("You Win!")
+    elif user_pick == 2 and computer_pick == 1:
+       print("You Win!")
+    elif user_pick == 1 and computer_pick == 0:
+        print("You Win")
+    elif user_pick == computer_pick:
+       print("DRAW!")
+    else:
+       print("You LOSE!")
+
+
+
